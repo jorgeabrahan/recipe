062137836a34ec81f971baf5dfef39ec4e4317c4
Amen TETTEH <tkamen@outlook.com>
2023-05-25 23:46:46 +0000

    Update file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+  # devise_scope :user do
+  #   root to: "devise/sessions#new"
+  # end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  # root "devise/sessions#create"
 end
