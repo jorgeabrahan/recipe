ef33dbb1b3c9b80585b6f90afd151c3d83e617dc
jorgeabrahan <jorge24abrahan@gmail.com>
2023-05-25 10:40:19 -0600

    Fix rubocop linter errors

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,9 +1,7 @@
 class RecipesController < ApplicationController
-  def index
-  end
+  def index; end
 
-  def show
-  end
+  def show; end
 
   def public_recipes
     @recipes = Recipe.where(public: true).order(created_at: :desc)
--- spec/requests/recipes_spec.rb
+++ spec/requests/recipes_spec.rb
@@ -1,25 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe "Recipes", type: :request do
-  describe "GET /new" do
-    it "returns http success" do
-      get "/recipes/new"
+RSpec.describe 'Recipes', type: :request do
+  describe 'GET /new' do
+    it 'returns http success' do
+      get '/recipes/new'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /index" do
-    it "returns http success" do
-      get "/recipes/index"
+  describe 'GET /index' do
+    it 'returns http success' do
+      get '/recipes/index'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /show" do
-    it "returns http success" do
-      get "/recipes/show"
+  describe 'GET /show' do
+    it 'returns http success' do
+      get '/recipes/show'
       expect(response).to have_http_status(:success)
     end
   end
-
 end
