39559add79359cea8b3484dd4fff7ab62649ef68
jorgeabrahan <jorge24abrahan@gmail.com>
2023-05-25 10:28:00 -0600

    Remove route configuration for new and create actions in the recipes resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "foods#index"
   resources :foods, only: [:index, :new, :create, :destroy]
-  resources :recipes, only: [:index, :new, :create, :show, :destroy]
+  resources :recipes, only: [:index, :show, :destroy]
   get '/public_recipes', to: 'recipes#public_recipes'
 end
