4e77760534fa13c5945efd24bd4fdf9a4787024b
jorgeabrahan <jorge24abrahan@gmail.com>
2023-05-25 10:30:33 -0600

    Order the public recipes by newest

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,6 +6,6 @@ class RecipesController < ApplicationController
   end
 
   def public_recipes
-    @recipes = Recipe.where(public: true)
+    @recipes = Recipe.where(public: true).order(created_at: :desc)
   end
 end
