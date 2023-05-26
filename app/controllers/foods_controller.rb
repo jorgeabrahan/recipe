49d5062d45f401e32ee65a38840da71332dde6c7
jorgeabrahan <jorge24abrahan@gmail.com>
2023-05-26 14:43:31 -0600

    Replace foods array from all foods to just the foods of the current user, and if the user is not logged in don't display redirect to sign in

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,8 +1,11 @@
 class FoodsController < ApplicationController
   def index
     # foods
-    @foods = Food.all # TODO: implement current_user method and remove this line
-    # @foods = current_user.foods
+    unless user_signed_in?
+      # should not display the foods of the logged in user if the user is not logged in!
+      redirect_to '/users/sign_in'
+    end
+    @foods = current_user.foods
   end
 
   def new
