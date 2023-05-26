72111fba94c27d78bc75bc37b03296c6ea7c5ca2
Amen TETTEH <tkamen@outlook.com>
2023-05-26 14:49:26 +0000

    Update to build recipe list

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   #   root to: "devise/sessions#new"
   # end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :recipes do
+    resources :recipe_foods
+  end
   # Defines the root path route ("/")
   # root "devise/sessions#create"
 end
