c25509ebb5e9efc973184057475a8faf10cf4db1
jorgeabrahan <jorge24abrahan@gmail.com>
2023-05-26 18:49:05 -0600

    Add validation to food model properties

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,8 @@
 class Food < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods, dependent: :destroy, foreign_key: 'food_id'
+
+  validates :name, presence: true, length: { maximum: 100 }
+  validates :measurement_unit, presence: true, length: { maximum: 100 }
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
 end
