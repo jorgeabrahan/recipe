d65b8079af331c117e9dc25aa85180496adc685c
jorgeabrahan <jorge24abrahan@gmail.com>
2023-05-26 18:51:10 -0600

    Change the default value of the quantity property in the recipe foods table

--- db/migrate/20230524045038_create_recipe_foods.rb
+++ db/migrate/20230524045038_create_recipe_foods.rb
@@ -4,7 +4,7 @@ class CreateRecipeFoods < ActiveRecord::Migration[7.0]
       t.references :food, foreign_key: true, index: true
       t.references :recipe, foreign_key: true, index: true
       
-      t.integer :quantity, default: 0
+      t.integer :quantity, default: 1
       
       t.timestamps
     end
