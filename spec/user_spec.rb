67b2c3a7fe9b0c1b443f2cea3ed8822d8346611e
Amen TETTEH <tkamen@outlook.com>
2023-05-27 19:25:32 +0000

    Update user_spec.rb file

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -10,8 +10,12 @@ RSpec.describe User, type: :model do
       user = User.new(name: 'Amen')
       expect(user).to_not be_valid
     end
+    it 'Ensure password presence' do
+      user = User.new(password: '123456')
+      expect(user).to_not be_valid
+    end
     it 'Should save successfully' do
-      user = User.new(name: 'Amen', email: 'amenior06@gmail.com')
+      user = User.new(name: 'Amen', email: 'amenior06@gmail.com',password: '123456')
       expect(user).to be_valid
     end
   end
