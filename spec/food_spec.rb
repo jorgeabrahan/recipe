2512528ae71ef13cf5d3fda650708747ce7b766d
jorgeabrahan <jorge24abrahan@gmail.com>
2023-05-26 20:31:08 -0600

    Create before each test method to create necessary instances to create a food

--- spec/food_spec.rb
+++ spec/food_spec.rb
@@ -1,4 +1,9 @@
 require 'spec_helper'
 
 RSpec.describe Food, type: :model do
+  before(:each) do
+    @not_valid_food = Food.new
+    @user = User.new(name: 'Juan')
+    @food = Food.new(user: @user, name: 'Apple', measurement_unit: 'grams', price: 2)
+  end
 end
