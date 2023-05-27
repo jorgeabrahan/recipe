03d6fb41fb012100b8bb937ab8d3cc35d6c0187b
jorgeabrahan <jorge24abrahan@gmail.com>
2023-05-26 20:36:07 -0600

    Create before each test method to create necessary instances to create a recipe food

--- spec/recipe_food_spec.rb
+++ spec/recipe_food_spec.rb
@@ -1,4 +1,11 @@
 require 'spec_helper'
 
 RSpec.describe RecipeFood, type: :model do
+  before(:each) do
+    @user = User.new(name: 'Juan')
+    @food = Food.new(user: @user, name: 'Apple', measurement_unit: 'grams', price: 2)
+    @recipe = Recipe.new(user: @user, name: 'Recipe', preparation_time: 60, cooking_time: 140, description: 'Recipe description', public: false)
+    @not_valid_recipe_food = RecipeFood.new(food: @food, recipe: @recipe)
+    @recipe_food = RecipeFood.new(food: @food, recipe: @recipe, quantity: 5)
+  end
 end
