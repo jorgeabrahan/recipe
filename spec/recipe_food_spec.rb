17c5f8271442627a309c33162f11ec4d27eb63ab
jorgeabrahan <jorge24abrahan@gmail.com>
2023-05-26 20:38:23 -0600

    Create tests to check if validations of recipe food model are working

--- spec/recipe_food_spec.rb
+++ spec/recipe_food_spec.rb
@@ -8,4 +8,16 @@ RSpec.describe RecipeFood, type: :model do
     @not_valid_recipe_food = RecipeFood.new(food: @food, recipe: @recipe)
     @recipe_food = RecipeFood.new(food: @food, recipe: @recipe, quantity: 5)
   end
+
+  it 'Should not be valid if required data is not supplied' do
+    expect(@not_valid_recipe_food).to_not be_valid
+  end
+  it 'Should not be valid if recipe food quantity is less than 1' do
+    @recipe_food.quantity = 0
+    expect(@recipe_food).to_not be_valid
+  end
+  it 'Should not be valid if recipe food quantity is not a number' do
+    @recipe_food.quantity = 'Hello'
+    expect(@recipe_food).to_not be_valid
+  end
 end
